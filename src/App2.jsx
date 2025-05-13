db88a3337ff6091bbe7ea64556b692436c5dca07
João Luca Rodrigues <jojoaoluca02@gmail.com>
2025-05-13 20:10:39 -0300

    Upando o front-end do projeto

---
+++ src/App2.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+// rafce
+
+// Nomeação de Componente = PascalCase
+// Nomeação de Variável, Função = camelCase
+
+// Nomeação de classes em CSS = Metodologia BEM
+// Block
+// Elements
+// Modifiers
+// bloco__elemento   elemento--modificador
+
+// header
+// header__link
+// header__link--small
+
+// Tag vazia em react se chama fragment ou fragmento
+
+// Componentes recebem "props"
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemList from "./ItemList";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <ItemList title="Artistas" items={5} />
+
+      <ItemList title="Músicas" items={10} />
+    </div>
+  );
+};
+
+export default Main;
