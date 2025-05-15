dbffb50644d4596ce5f9c0969533059cde6a0d77
João Luca Rodrigues <jojoaoluca02@gmail.com>
2025-05-14 22:10:49 -0300

    Added router and organized estructure

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
-const SingleItem = () => {
+const SingleItem = ({ id, name, image, artist, idPath }) => {
   return (
-    <div className="single-item">
+    <Link to={`${idPath}/${id}`} className="single-item">
       <div className="single-item__div-image-button">
         <div className="single-item__div-image">
           <img
             className="single-item__image"
-            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
-            alt="Imagem do artista X"
+            src={image}
+            alt={`Imagem do artista ${name}`}
           />
         </div>
         <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay} />
       </div>
       <div className="single-item__texts">
-        <p className="single-item__title">Henrique & Juliano</p>
-        <p className="single-item__type">Artista</p>
+        <p className="single-item__title">{name}</p>
+        <p className="single-item__type">
+          {artist ?? "Artista"}
+          {/* {artist === undefined ? "Artista" : artist} */}
+        </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
